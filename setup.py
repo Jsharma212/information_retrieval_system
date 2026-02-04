6456d1590d16600e606f8dcb5b2f2f20d5967f97
Jsharma212 <111627321+Jsharma212@users.noreply.github.com>
2026-02-05 02:50:11 +0530

    Requirements.txt file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version="0.0.0",
+    author="Jyotish",
+    author_email="jsharma212@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
